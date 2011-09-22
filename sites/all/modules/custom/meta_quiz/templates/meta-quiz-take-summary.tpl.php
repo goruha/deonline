67ff8456a323df484c37b9014fb1fba26a8f9385
Igor Rodionov <goruha@gmail.com>
2011-09-22 21:15:18 +0700

    add meta test middle

---
+++ sites/all/modules/custom/meta_quiz/templates/meta-quiz-take-summary.tpl.php
@@ -0,0 +1,10 @@
+<div id="quiz_score_possible">
+  <?php print $score_possible; ?>
+</div>
+<div id="quiz_score_percent">
+  <?php print $score_percent; ?>
+</div>
+<div id="quiz_summary">
+  <?php print $message; ?>
+</div>
+<?php print $form; ?>
